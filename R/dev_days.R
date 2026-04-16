895932b0ff68242d1d53f20111a182aba0ac90fd
Kala Studens <kala.studens@gmail.com>
2026-04-16 15:29:21 -0400

    Add Imports

--- R/dev_days.R
+++ R/dev_days.R
@@ -38,17 +38,17 @@ dev_days <- function(weather,
   }
 
   weather.clean <- weather |>
-    mutate(Date = as.Date(Date),
+    dplyr::mutate(Date = as.Date(Date),
            Temp = pmax(Temp, 0),
-           DateTime = Date + hours(Hour),
-           Year = year(Date),
-           JDay = yday(Date)) |>
-    arrange(DateTime)
+           DateTime = Date + lubridate::hours(Hour),
+           Year = lubridate::year(Date),
+           JDay = lubridate::yday(Date)) |>
+    dplyr::arrange(DateTime)
   all.dates <- weather.clean |>
-    select(DateTime) |>
-    reframe(DateTime = seq(min(DateTime), max(DateTime), by = period)) |>
-    left_join(weather.clean, by = join_by(DateTime)) |>
-    subset(is.na(Temp)) |>
+    dplyr::select(DateTime) |>
+    dplyr::reframe(DateTime = seq(min(DateTime), max(DateTime), by = period)) |>
+    dplyr::left_join(weather.clean, by = join_by(DateTime)) |>
+    dplyr::subset(is.na(Temp)) |>
     unique() |>
     nrow()
   if (all.dates > 0) {
@@ -57,8 +57,8 @@ dev_days <- function(weather,
                    ifelse(all.dates > 1, 's', '')))
   }
 
-  info.sub <- subset(curve_info, colony == colony) |>
-    arrange(run.index, stage)
+  info.sub <- dplyr::subset(curve_info, colony == colony) |>
+    dplyr::arrange(run.index, stage)
   inds <- sort(info.sub$index)
   run.inds <- sort(sample(unique(info.sub$run.index),
                           n.post, replace = FALSE))
@@ -70,13 +70,13 @@ dev_days <- function(weather,
   isplines <- matrix(nrow = nweather, ncol = length(stages))
 
   for (i in seq(run.inds)) {
-    sub <- subset(info.sub, run.index == run.inds[i])
+    sub <- dplyr::subset(info.sub, run.index == run.inds[i])
     new.inds <- sub$index
 
     for (j in seq(new.inds)) {
       ind <- new.inds[[j]]
-      isplines[,j] <- with(subset(curves, index == ind), {
-        spl <- interpSpline(temp, rate/div)
+      isplines[,j] <- with(dplyr::subset(curves, index == ind), {
+        spl <- splines::interpSpline(temp, rate/div)
         predict(spl, weather.clean$Temp)$y
       })
     }
